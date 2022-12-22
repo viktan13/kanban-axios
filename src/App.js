05ba8c75c4756b6d13693479abce14520cd6a450
Viktor <vik.tan@hotmail.com>
2022-12-21 22:29:00 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const getStatuses = () => {
+        axios.get('http://localhost:5000/cards')
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        getStatuses();
+    }, [])
+
+    return (
+        <div>
+            <h1>Kanban Board + Axios</h1>
+
+        </div>
+    );
 }
 
 export default App;
